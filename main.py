e6c97bf7f18d480ab625d62ea5aba7b02525127b
KevinStock <kevin@kevinstock.net>
2016-08-19 22:26:28 -0500

    caesar successful with memory of rotation value

--- main.py
+++ main.py
@@ -30,10 +30,11 @@ import cgi
 form = """
     <form method="post">
         <label>
-            <strong>Enter some text to ROT13:</strong>
-            <br />
-            <textarea autofocus required rows="10" cols="80" name="rot_textarea">%(rot_text)s</textarea>
+            <strong>Rotate by:</strong>
+            <input type="number" name="rotate" value="%(rotate)s" />
         </label>
+        <br />
+        <textarea autofocus required rows="10" cols="80" name="rot_textarea">%(rot_text)s</textarea>
         <br />
         <input type="submit" />
     </form>
@@ -50,15 +51,17 @@ def escape_html(s):
 
 class MainHandler(webapp2.RequestHandler):
     
-    def write_form(self, rot_text=""):
-        self.response.out.write(form % {"rot_text": escape_html(rot_text)})
+    def write_form(self, rot_text="", rotation="0"):
+        self.response.out.write(form % {"rot_text": escape_html(rot_text),
+                                        "rotate": rotation})
     
     def get(self):
         self.write_form()
 
     def post(self):
         rot_text = self.request.get("rot_textarea")
-        self.write_form(encrypt(rot_text, 13))
+        rotation = self.request.get("rotate")
+        self.write_form(encrypt(rot_text, int(rotation)), rotation)
     
 
 app = webapp2.WSGIApplication([
